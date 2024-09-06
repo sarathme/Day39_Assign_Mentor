90cd11fb1c7fbbd918e76b1f28345370efe7029b
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-06 19:22:41 +0530

    Create global error handling middleware function and middleware

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 
+const mentorRouter = require("./routes/mentorRoutes");
+const globalErrorHandler = require("./controllers/errorController");
+
 const app = express();
 
+// Middleware to attach body to request object and parse JSON.
 app.use(express.json());
 
+// ROUTES
+
+app.use("/api/v1/mentors", mentorRouter);
+
+app.use(globalErrorHandler);
 module.exports = app;
