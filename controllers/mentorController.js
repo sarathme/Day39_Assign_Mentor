32d8ec1ee1a3496f67f8e95520d24ea765789741
Sarasraman <sarathsatheesh603@gmail.com>
2024-09-07 18:17:34 +0530

    Add route to add one student to a mentor and add route to get all mentors

--- controllers/mentorController.js
+++ controllers/mentorController.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const { catchAsync } = require("../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
@@ -6,8 +6,21 @@ const url = process.env.DB_CONNECTION.replace(
   "<PASSWORD>",
   process.env.DB_PASSWORD
 );
-
 const client = new MongoClient(url);
+
+exports.getAllMentors = catchAsync(async (req, res, next) => {
+  await client.connect();
+  const database = client.db("guvi_mentor");
+  const mentorsCollection = database.collection("mentors");
+  const mentors = await mentorsCollection.find({}).toArray();
+  await client.close();
+  res.status(200).json({
+    status: "success",
+    data: {
+      mentors,
+    },
+  });
+});
 
 exports.createMentor = catchAsync(async (req, res, next) => {
   await client.connect();
@@ -33,6 +46,80 @@ exports.createMentor = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: "success",
     message: "Mentor created successfully",
+    data: {
+      mentor,
+    },
+  });
+});
+
+exports.addStudent = catchAsync(async (req, res, next) => {
+  await client.connect();
+  const database = client.db("guvi_mentor");
+  const mentors = database.collection("mentors");
+  const students = database.collection("students");
+
+  // This try catch is to handle if invalid format of Object id is specified
+  let studentObjectId;
+  let mentorObjectId;
+  try {
+    studentObjectId = new ObjectId(req.params.studentId);
+  } catch (err) {
+    console.error("Error: ", err);
+    next(new AppError("No student found with the provided id", 404));
+    await client.close();
+    return;
+  }
+
+  try {
+    mentorObjectId = new ObjectId(req.params.mentorId);
+  } catch (err) {
+    console.error("Error: ", err);
+    next(new AppError("No mentor found with the provided id", 404));
+    await client.close();
+    return;
+  }
+
+  const student = await students.findOne({
+    _id: studentObjectId,
+  });
+
+  if (!student) {
+    next(new AppError("No student found with the provided id", 404));
+    await client.close();
+    return;
+  } else if (student.mentorId) {
+    next(
+      new AppError(
+        "The provided student is already assigned with a mentor. Try updating the mentor on behalf of the student",
+        400
+      )
+    );
+    await client.close();
+    return;
+  }
+
+  let mentor = await mentors.findOneAndUpdate(
+    {
+      _id: mentorObjectId,
+    },
+    { $push: { students: req.params.studentId } }
+  );
+
+  if (!mentor) {
+    next(new AppError("No mentor found with the provided id", 404));
+    await client.close();
+    return;
+  }
+  await students.findOneAndUpdate(
+    { _id: studentObjectId },
+    { $set: { mentorId: mentorObjectId } }
+  );
+  mentor = await mentors.findOne({
+    _id: mentorObjectId,
+  });
+
+  res.status(200).json({
+    status: "success",
     data: {
       mentor,
     },
--- routes/mentorRoutes.js
+++ routes/mentorRoutes.js
@@ -4,6 +4,12 @@ const mentorController = require("./../controllers/mentorController");
 
 const router = express.Router();
 
-router.route("/").post(mentorController.createMentor);
+router
+  .route("/")
+  .post(mentorController.createMentor)
+  .get(mentorController.getAllMentors);
+router
+  .route("/:mentorId/students/:studentId")
+  .patch(mentorController.addStudent);
 
 module.exports = router;
